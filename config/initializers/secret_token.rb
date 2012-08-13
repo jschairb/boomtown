b0e5bd7f44b0dbaccfd454e785aab6e59005d067
Josh Schairbaum <josh.schairbaum@rackspace.com>
2012-08-12 21:50:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoomTown::Application.config.secret_token = 'a152c624b2ad2956f2320cb2652745223ddcfb1dae8bc74b27c062b163b15d7ff286fdc06d34e3a48b06be2441ce2f08c73b1cf2c6cd240044fe30a3fa710c4c'
